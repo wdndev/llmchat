a1f34b1823d0d3441acd5e4fcee55f437925d677
wdndev <dongnian.wang@outlook.com>
2025-06-19 15:17:03 +0800

    message pinia

---
+++ src/stores/message.ts
@@ -0,0 +1,50 @@
+import {defineStore} from 'pinia'
+import {db} from '../db'
+import {MessageProps, MessageStatus,  UpdatedStreamData} from '../types'
+
+export interface MessageStore {
+    items: MessageProps[]
+}
+
+export const useMessageStore = defineStore('message', {
+    state: (): MessageStore => {
+        return {
+            items: []
+        }
+    },
+    actions: {
+        async fetchMessagesByConversationId(conversationId: number) {
+            const messages = await db.messages.where({conversationId}).toArray()
+            this.items = messages
+        },
+        async createMessage(data: Omit<MessageProps, 'id'>) {
+            const newMsgId = await db.messages.add(data)
+            this.items.push({
+                ...data,
+                id: newMsgId
+            })
+            return newMsgId
+        },
+        async updateMessage(streamData: UpdatedStreamData) { 
+            const { messageId, data } = streamData
+            const currentMessage = this.items.find(item => item.id === messageId)
+            if (currentMessage) {
+                const updatedData = {
+                    content: currentMessage.content + data.result,
+                    status: data.is_end ? 'finished' : 'streaming' as MessageStatus,
+                    updatedAt: new Date().toISOString()
+                }
+                await db.messages.update(messageId, updatedData)
+                const index  = this.items.findIndex(item => item.id === messageId)
+                if (index !== -1) {
+                    this.items[index] = {...this.items[index], ...updatedData}
+                }
+            }
+        },
+    },
+    getters: {
+        getLastQuestion: (state) => (conversationId: number) => {
+            return state.items.findLast(item => item.conversationId === conversationId && item.type === 'question')
+        }
+    }
+})
--- src/views/Conversation.vue
+++ src/views/Conversation.vue
@@ -19,21 +19,27 @@
     import MessageInput from '../components/MessageInput.vue'
     import MessageList from '../components/MessageList.vue'
     import { useConversationStore } from '../stores/conversation';
+    import { useMessageStore } from '../stores/message';
     import { db } from '../db'
-import { c } from 'vite/dist/node/types.d-aGj9QkWt';
+
     // 测试数据
     // import { messages, conversations } from '../testData'
 
     const route = useRoute()
     const conversationStore = useConversationStore()
-    const filteredMessages = ref<MessageProps[]>([])
+    const messageStore = useMessageStore()
+    // const filteredMessages = ref<MessageProps[]>([])
+
     // const conversation = ref<ConversationProps>()
     let conversationId = ref(parseInt(route.params.id as string))
     const initMessageId = parseInt(route.query.init as string)
     const conversation = computed(() => {
         return conversationStore.getConversationById(conversationId.value)
     })
-    let lastQuestion = ''
+    const filteredMessages = computed(() => {
+        return messageStore.items
+    })
+    const lastQuestion = computed(() => messageStore.getLastQuestion(conversationId.value))
 
     const creatingInitialMessage = async () => {
         const createdData: Omit<MessageProps, 'id'> = {
@@ -44,11 +50,7 @@ import { c } from 'vite/dist/node/types.d-aGj9QkWt';
             createdAt: new Date().toISOString(),
             updatedAt: new Date().toISOString(),
         }
-        const newMessageId = await db.messages.add(createdData)
-        filteredMessages.value.push({
-            ...createdData,
-            id: newMessageId
-        })
+        const newMessageId = await messageStore.createMessage(createdData)
         if (conversation.value) {
             const provider = await db.providers.where({id: conversation.value.providerId}).first()
             if (provider) {
@@ -56,7 +58,7 @@ import { c } from 'vite/dist/node/types.d-aGj9QkWt';
                     messageId: newMessageId,
                     providerName: provider.name,
                     selectedModel: conversation.value.selectedModel,
-                    content: lastQuestion
+                    content: lastQuestion.value?.content || ''
                 })
             }
         }
@@ -67,38 +69,39 @@ import { c } from 'vite/dist/node/types.d-aGj9QkWt';
     
     watch(() => route.params.id, async (newID: string) => {
         conversationId.value = parseInt(newID)
-        filteredMessages.value = await db.messages.where({conversationId: conversationId.value}).toArray()
+        await messageStore.fetchMessagesByConversationId(conversationId.value)
+        // filteredMessages.value = await db.messages.where({conversationId: conversationId.value}).toArray()
         // conversation.value = await db.conversations.where({id: conversationId}).first()
     })
 
     onMounted(async () => { 
         // conversation.value = await db.conversations.where({id: conversationId}).first()
-        filteredMessages.value = await db.messages.where({conversationId: conversationId.value}).toArray()
+        // filteredMessages.value = await db.messages.where({conversationId: conversationId.value}).toArray()
+        await messageStore.fetchMessagesByConversationId(conversationId.value)
         if (initMessageId) {
-            const lastMessage = await db.messages.where({conversationId: conversationId.value}).last()
-            lastQuestion = lastMessage?.content || ''
             await creatingInitialMessage()
         }
         window.electronAPI.onUpdateMessage(async (streamData) => {
             console.log('streamData', streamData)
             // 更新数据库 + 更新页面
-            const {messageId, data} = streamData
-            const currentMessage = await db.messages.where({id: messageId}).first()
-            if (currentMessage) {
-                const updatedData= {
-                    content: currentMessage.content + data.result,
-                    status: data.is_end ? 'finished' : 'streaming' as MessageStatus,
-                    updatedAt: new Date().toISOString(),
-                }
-                // 数据库更新
-                await db.messages.update(messageId, updatedData)
-                // 页面更新
-                const index = filteredMessages.value.findIndex(item => item.id === messageId)
-                if (index !== -1) {
-                    filteredMessages.value[index] = {...filteredMessages.value[index], ...updatedData}
-                }
+            messageStore.updateMessage(streamData)
+            // const {messageId, data} = streamData
+            // const currentMessage = await db.messages.where({id: messageId}).first()
+            // if (currentMessage) {
+            //     const updatedData= {
+            //         content: currentMessage.content + data.result,
+            //         status: data.is_end ? 'finished' : 'streaming' as MessageStatus,
+            //         updatedAt: new Date().toISOString(),
+            //     }
+            //     // 数据库更新
+            //     await db.messages.update(messageId, updatedData)
+            //     // 页面更新
+            //     const index = filteredMessages.value.findIndex(item => item.id === messageId)
+            //     if (index !== -1) {
+            //         filteredMessages.value[index] = {...filteredMessages.value[index], ...updatedData}
+            //     }
 
-            }
+            // }
         })
     })
 
